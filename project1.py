22903b020fe3c47efcae24b170baf68d99e2a1f6
John Radcliffe <guitarplayer3250@gmail.com>
2015-04-13 17:11:49 -0700

    Added file systems array, new function
    The file systems dictionary uses the hex code as a key with the name of
    the partition type as the value.
    The new function partitionEntry takes an entry and displays it's
    information following the the entry format.

--- project1.py
+++ project1.py
@@ -5,6 +5,19 @@ import binascii
 
 md5hash = hashlib.md5()
 sha1hash = hashlib.sha1()
+filesystems = {'01':'DOS 12-BIT FAT',
+			   '04':'DOS 16-BIT FAT',
+			   '05':'Extended Partition',
+			   '06':'DOS 16-BIT FAT',
+			   '07':'NTFS',
+			   '08':'AIX Bootable Partition',
+			   '09':'AIX Data Partition',
+			   '0b':'DOS 32-BIT FAT',
+			   '0c':'DOS 32-BIT FAT',
+			   '17':'Hidden NTFS Parition (XP and earlier',
+			   '1b':'Hidden FAT32 Partition',
+			   '1e':'Hidden VFAT Partition',
+			   '3c':'Partition Magic Recovery Partition'}
 
 # Filepath is hard coded to cut down on test time
 #filepath = input("Enter file path: ")
@@ -21,12 +34,33 @@ print(md5hash.hexdigest())
 print(sha1hash.hexdigest())
 """
 
+def partitionEntry(entry):
+	"Displays parition entry information"
+	print(binascii.hexlify(entry[0]))
+	print(binascii.hexlify(entry[1]))
+	print(binascii.hexlify(entry[2:4]))
+	print(filesystems[binascii.hexlify(entry[4])])
+	print(binascii.hexlify(entry[5]))
+	print(binascii.hexlify(entry[6:8]))
+	print(binascii.hexlify(entry[11:7:-1]))
+	print(binascii.hexlify(entry[15:11:-1]))
+	return
+
 # Analysis Section
 # **Change filepath if img is not in the same directory
-with open('TestImage1.img', 'rb') as f:
-	block = f.read(48*2)
-#print(block)
-#print(binascii.hexlify(block))
-vbr = binascii.hexlify(block[0:36*2])
-for x in range(0,80,32):
-	print("{} {}".format(vbr[x:x+16], vbr[x+16:x+32]))
+with open('C:\Users\Jesse Laptop\Desktop\TestImage1.img', 'rb') as f:
+	block = f.read(512)
+executionCode = block[0:446]
+firstPartition = block[446:462]
+secondPartition = block[462:478]
+thirdPartiion = block[478:494]
+fourthPartiion = block[494:510]
+brs = block[510:512]
+
+print(binascii.hexlify(firstPartition))
+print(binascii.hexlify(secondPartition))
+print(binascii.hexlify(thirdPartiion))
+print(binascii.hexlify(fourthPartiion))
+print(binascii.hexlify(brs))
+partitionEntry(firstPartition)
+partitionEntry(secondPartition)
