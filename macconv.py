8d12e8cbdb5a4f797ce83e5c5be512eeee0e780b
John Radcliffe <guitarplayer3250@gmail.com>
2015-04-14 17:17:25 -0700

    MAC Conversion Fix and Changes
    -Changed -h to -x because it causes some strange behavior with docopt
    -added some test code for opening and reading a file

--- macconv.py
+++ macconv.py
@@ -2,15 +2,15 @@
 
 Usage:
   macconv.py -T -f FILENAME
-  macconv.py -T -h VAL
+  macconv.py -T -x VAL
   macconv.py -D -f FILENAME
-  macconv.py -D -h VAL
+  macconv.py -D -x VAL
 
 Options:
   -T  Uses time conversion module.
   -D  Uses date conversion module.
   -f FILENAME --file=FILENAME  Takes a file containing hex value to convert.
-  -h VAL --hex=VAL  Takes a hex value to convert.
+  -x VAL --hex=VAL  Takes a hex value to convert.
 
 """
 
@@ -20,3 +20,8 @@ from docopt import docopt
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Address Conversion Utility 1.0')
     print(arguments)
+
+if arguments['-T']:
+  with open('C:\Users\Jesse Laptop\Desktop\example.txt', 'r') as f:
+    hexcode = f.read()
+    print(hexcode)
