b367085fe77ba6efb120415f47e4c491633d459d
nicolesquivel03 <nicolesquivel03@users.noreply.github.com>
2015-04-21 21:59:48 -0700

    Update macconv.py
    Added functions to convert hex into date and time as well as added functions to open files and convert hex in files to date and time.

--- macconv.py
+++ macconv.py
@@ -14,44 +14,79 @@ Options:
 
 from docopt import docopt
 
+dir(bytes)
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Address Conversion Utility 1.0')
-    print(arguments)
+
+
+#hex_time1 = b'53 f6';
+#10:31:44
+#hex_time1 = hex_time1.replace(b' ',b'');
+
+#hex_date1 = b'42 4f';
+#Feb 15, 2013
+#hex_date1 = hex_date1.replace(b' ', b'');
+
+def calctime(value):
+    hex_time = value
+    val1 = int(hex_time, 16)
+    seconds = ((val1 & 0b11111)*2)
+    minutes = ((val1 >> 5) & 0b111111)
+    hours = ((val1 >> 11) & 0b11111)
+    print(str(hours) + ':' + str(minutes) + ':' + str(seconds))
+    return
+
+
+def calcdate(value):
+    hex_date = value
+    val2 = int(hex_date, 16)
+    day = (val2 & 0b11111)
+    monthnum = ((val2 >> 5) & 0b1111)
+    year = (((val2 >> 9) & 0b1111111) + 1980)
+    if monthnum is 1:
+        month = 'Jan'
+    elif monthnum is 2:
+        month = 'Feb'
+    elif monthnum is 3:
+        month = 'Mar'
+    elif monthnum is 4:
+        month = 'Apr'
+    elif monthnum is 5:
+        month = 'May'
+    elif monthnum is 6:
+        month = 'June'
+    elif monthnum is 7:
+        month = 'July'
+    elif monthnum is 8:
+        month = 'Aug'
+    elif monthnum is 9:
+        month = 'Sep'
+    elif monthnum is 10:
+        month = 'Oct'
+    elif monthnum is 11:
+        month = 'Nov'
+    elif monthnum is 12:
+        month = 'Dec'
+    else:
+        print('Error Calculating Month')
+        return
+
+    print(month + ' ' + str(day) + ', ' + str(year))
+    return
+
 
 if arguments['-T']:
-  if arguments['--file'] != None:
-    print('-T -f')
-  elif arguments['--hex'] != None:
-    print('-T -x')
+    if arguments['--file'] is not None:
+        with open(arguments['--file'], 'rb') as f:
+            fileContents = f.read()
+            calctime(fileContents)
+    elif arguments['--hex'] is not None:
+        calctime(arguments['--hex'])
 elif arguments['-D']:
-  if arguments['--file'] != None:
-    print('-D -f')
-  elif arguments['--hex'] != None:
-    print('-D -x')
-    
-dir(bytes);
-
-hex_time1 = b'53 f6';
-
-hex_time1 = hex_time1.replace(b' ',b'');
-
-
-print(hex_time1);
-
-#10:31:44
-val1 = int(hex_time1, 16);
-print((val1 & 0b11111)*2);
-print((val1 >> 5) & 0b111111);
-print((val1 >> 11) & 0b11111);
-
-hex_date1 = b'42 4f';
-hex_date1 = hex_date1.replace(b' ', b'');
-
-print(hex_date1);
-
-#Feb 15, 2013
-val2 = int(hex_date1, 16);
-print(val2 & 0b11111);
-print((val2 >> 5) & 0b1111);
-print(((val2 >> 9) & 0b1111111) + 1980);
+    if arguments['--file'] is not None:
+        with open(arguments['--file'], 'rb') as f:
+            fileContents = f.read()
+            calcdate(fileContents)
+    elif arguments['--hex'] is not None:
+        calcdate(arguments['--hex'])
